55d0427cdc050a9ef273b3731db1141d0f27980b
Nikky <nikky.runghen.vezina@gmail.com>
2025-03-08 10:29:30 -0500

    base structure typescript react frontend

---
+++ game/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TestComponent from './TestComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TestComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
