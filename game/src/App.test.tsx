4681bf40df455085a0a0df20ab245bcbb71cdbfb
Nikky <nikkyeryn@gmail.com>
2025-03-08 23:31:16 -0500

    Updates

--- game/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import TestComponent from './TestComponent';
-
-test('renders message input', () => {
-  render(<TestComponent />);
-  const inputElement = screen.getByPlaceholderText(/enter message/i);
-  expect(inputElement).toBeInTheDocument();
-});
